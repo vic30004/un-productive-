736b6dbe41903c46b20f8778a54de91a94b29873
Angel Cochachez <acochachez@gmail.com>
2019-10-18 21:24:17 -0700

    Fixing last error

--- un-productive/src/component/books/pages/BooksItems.js
+++ un-productive/src/component/books/pages/BooksItems.js
@@ -21,11 +21,11 @@ BooksItems.propTypes = {
 }
 export default BooksItems;*/
 import React, {Component} from 'react';
-import ajax from 'supertest';
+import request from 'supertest';
+/*import Books from './component/books/pages/Books';*/
 import logo from './images/Un-productive.png';
 import './App.css';
 import 'font-awesome/css/font-awesome.css';
-
 import { tsConstructorType } from '@babel/types';
 
 class App extends Component{
@@ -35,7 +35,7 @@ class App extends Component{
         books:[],
         loading:false,
       /*games:[],*/
-        slideCount:0
+        slideCount:0,
       }
     }
 
@@ -49,7 +49,7 @@ class App extends Component{
       .then(data=>{
         console.log(data.items);
         this.setState({books:data.items, loading:false});
-    });
+    })
   }
   
   render(){
@@ -59,45 +59,46 @@ class App extends Component{
           <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Slideshow</h1>
         </header>
+        
 
-        /* Displaying the images */
-        <div>
-        {this.state.books.map((photo, key)=> {
-          return(
-            <div key={photo.id}>
-              <img src={photo.images.standard_resolution.url} alt={photo.caption}/>
+          /* Displaying the images in slideshow*/
+          <div>
+          {this.state.books.map((photo, key)=> {
+            return(
+              <div key={photo.id}>
+                <img src={photo.images.standard_resolution.url} alt={photo.caption}/>
+              </div>
+            )
+          })}
+          </div>
+
+          /* Display caption in our books*/
+          <div style={{width:'600px', margin:'24px auto', fontstyle: 'italic'}}>
+            {logo.caption !== null ? logo.caption.text: ''}
+          </div>
+
+          /* Adding the slider arrows*/
+          const BackArrow=(props)=>(
+            <div style={{fontsize: '2em', marginRight:'12px'}}>
+              <i className="fa fa-angle-left fa-2x" aria-hidden="true"></i>
             </div>
           )
-        })}
-        </div>
 
-        /* Display caption in our books*/
-        <div style={{width:'600px', margin:'24px auto', fontstyle: 'italic'}}>
-          {logo.caption !== null ? logo.caption.text: ''}
-        </div>
+          const NextArrow=(props)=>(
+            <div style={{fontsize:'2em', marginleft:'12px'}}>
+              <i className="fa fa-angle-left fa-2x" aria-hidden="true"></i> 
+            </div>
+            )
 
-        /* Adding the slider arrows*/
-        const BackArrow=()=>(
-          <div style={{fontsize: '2em', marginRight:'12px'}}>
-            <i className="fa fa-angle-left fa-2x" aria-hidden="true"></i>
-          </div>
-        )
-
-        const NextArrow=()=>(
-          <div style={{fontsize:'2em', marginleft:'12px'}}>
-            <i className="fa fa-angle-left fa-2x" aria-hidden="true"></i> 
-          </div>
-          )
-
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to my slideshow</h1>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to my slideshow</h1>
             </header>
             <div style={{display: 'flex', alignItems: 'center', justifyContent:'center', marginTop: '30px'}}>
-              {this.state.slideCount !==0 ? <BackArrow previousImage={this.previousImage}/> : ''}
-              {this.state.photos.map((photo,key)=>{
-                if(this.state.photos.indexOf(photo)===this.state.slideCount){
+              {this.state.slideCount !==0 ? <BackArrow previousImage={this.previousImage}/> : ''};
+              {this.state.books.map((photo, key)=>{
+                if(this.state.books.indexOf(photo) === this.state.slideCount){
                   return(
                     <div key={photo.id} style={{margin: '0 auto'}}>
                       <img src={photo.images.standard_resolution.url} alt=''/>
@@ -109,43 +110,37 @@ class App extends Component{
                 }
                 return ''
               })}
-              {this.state.slideCount !== (this.state.photos.lenght -1)? <NextArrow nextImage={this.nextImage}/>:''}</div>
+              {this.state.slideCount !== (this.state.books.lenght -1)? <NextArrow nextImage={this.nextImage}/>:''}</div>
 
               nextImage(){
-                this.setState({slideCount:this.state.slidecount + 1})
+              this.setState({slideCount:this.state.slidecount + 1})
                 }
               previousImage(){
-                this.setState({slideCount:this.state.slideCount - 1})
+              this.setState({slideCount:this.state.slideCount - 1})
                 }
               
               const BackArrow={props} => (
-                <div onClick={props.previousImage} style={{fonzise: '2em',marginRight:'12px'}}>
+                <div onClick={props.previousImage} style={{fontSize: '2em',marginRight:'12px'}}>
                   <i className="fa fa-angle-left fa-2x" aria-hidden="true"></i>
                 </div>
               )
-              const NextArrow=(props)=>(
+              const NextArrow=(props) => (
                 <div onClick={props.nextImage} style={{fontSize: '2em', marginLeft:'12px'}}>
                   <i className="fa fa-angle-right fa-2x" aria-hidden="true"></i>
                 </div>
               )
-
+              
               <BackArrow previousImage={this.previousImage}/>
               <NextArrow nextImage={this.nextImage}/>
 
-              this.nextImage-this.nextImage.bin(this);
-              this.previousImage=this.previousImage.bind(this);
-
-        <div>
-          {console.log(this.state.books)};
-        </div>
-      </div>
-      </div>
-    );
+              //1678826271.1677ed0.15c57fd844a74f45be4f7fa9de32a5e1
+        
+            <div>
+              {console.log(this.state.books)};
+            </div>
+        
+      };  
+    )
   }
-}
-
 
 export default App;
-
-
-
